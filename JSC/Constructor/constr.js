8d30af97200bf3bcd32ef4a3dacc53ef20c8c1c3
Neboraka <oleshchukii@gmail.com>
2022-05-17 12:19:03 +0300

    cinderellas life

--- JSC/Constructor/constr.js
+++ JSC/Constructor/constr.js
@@ -23,26 +23,102 @@
 // let sort = book.sort((a,b)=>a.id-b.id)
 // console.log(sort);
 
-function Car(model,producer,year,maxspeed,volume) {
-    this.model = model
-    this.producer = producer
-    this.year = year
-    this.maxspeed = maxspeed
-    this.volume = volume
-    this.drive = function () {
-        console.log(`We are dribing with ${maxspeed}m/h speed`);
-    }
-    this.info = function () {
-        for (const item in this) {
-            console.log(`${item} - ${this[item]}`);
-        }
-    }
-    this.faster = function (fust) {
-     this.maxspeed += fust
+// function Car(model,producer,year,maxspeed,volume) {
+//     this.model = model
+//     this.producer = producer
+//     this.year = year
+//     this.maxspeed = maxspeed
+//     this.volume = volume
+//     this.drive = function () {
+//         console.log(`We are dribing with ${maxspeed}m/h speed`);
+//     }
+//     this.info = function () {
+//         for (const item in this) {
+//             console.log(`${item} - ${this[item]}`);
+//         }
+//     }
+//     this.faster = function (fust) {
+//      this.maxspeed += fust
+//     }
+//     this.Year = function (newYear){
+//        return this.year = newYear
+//     }
+//     this.addDriver = function (driver){
+//         this.driver = driver
+//     }
+// }
+// let vol = new Car('V40','Volvo',2000,168,1.4)
+// // vol.drive()
+// // // vol.info()
+// // vol.faster(40);
+// // console.log(vol);
+// console.log(vol.Year(2030));
+// vol.addDriver('Andrii')
+// console.log(vol);
+
+// class Car {
+//     constructor(model,producer,year,maxspeed,volume) {
+//         this.model = model
+//         this.producer = producer
+//         this.year = year
+//         this.maxspeed = maxspeed
+//         this.volume = volume
+//         }
+//
+//         drive () {
+//         console.log(`We are driving with ${this.maxspeed}m/h speed`);
+//         }
+//         info () {
+//             for (const item in this) {
+//                 console.log(`${item} - ${this[item]}`);
+//             }
+//         }
+//         faster (fust) {
+//          this.maxspeed += fust
+//             return
+//         }
+//         yearn (newYear){
+//           return this.year = newYear
+//         }
+//         addDriver (driver){
+//             this.driver = driver
+//         }
+// }
+// let vol = new Car('V40','Volvo',2000,168,1.4)
+// // vol.drive()
+// // vol.info()
+// // vol.faster(40);
+// // console.log(vol);
+// vol.yearn(2030);
+// console.log(vol);
+// // vol.addDriver('Andrii')
+// // console.log(vol);
+
+function cinderella(name,age,footSize){
+    this.name = name
+    this.age = age
+    this.footSize = footSize
+}
+let arrCinderellas = [
+    new cinderella('Kasha', 68, 44),
+    new cinderella('Kapusta', 44, 51),
+    new cinderella('Babka', 78, 38),
+]
+function Prince(name,age,findSize){
+    this.name = name
+    this.age = age
+    this.findSize = findSize
+}
+let newPrince = new Prince('Anatolii', 24, 38)
+
+let find = (arrCind, newPr)=>{
+    for (const Cinde of arrCind) {
+        if(Cinde.footSize === newPr.findSize)
+            console.log(`This is the best ${Cinde.name}`)
     }
 }
-let vol = new Car('V40','Volvo',2000,168,1.4)
-// vol.drive()
-// vol.info()
-console.log(vol.faster(40));
-console.log(vol);
+
+find(arrCinderellas,newPrince)
+
+let cind = arrCinderellas.find(value => value.footSize === newPrince.findSize)
+console.log(cind);
