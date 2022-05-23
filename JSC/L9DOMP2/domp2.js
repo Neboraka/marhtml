8354c74822255bb5228b3396245472f10d093216
Neboraka <oleshchukii@gmail.com>
2022-05-23 20:30:23 +0300

    DOM2

---
+++ JSC/L9DOMP2/domp2.js
@@ -0,0 +1,22 @@
+// const div = document.createElement('div')
+// div.innerText = 'Testing block for lesson 9'
+// // div.classList.add('wrap')
+// // div.classList.add('collapse')
+// // div.classList.add('alpha')
+// // div.classList.add('beta')
+// div.setAttribute('class', 'wrap collapse alpha beta')
+// div.style.background = 'red'
+// div.style.color = 'white'
+// div.style.width = '200px'
+//
+//
+// document.body.append(div)
+// document.body.append(div.cloneNode(true))
+
+// const mas = ['Main','Products','About us','Contacts']
+// for (const masE of mas) {
+//     let elem = document.getElementsByClassName('menu')[0]
+//     const li = document.createElement('li')
+//     li.innerText = masE
+//     elem.appendChild(li)
+// }
