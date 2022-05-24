9cfeb6f886ba576bca8ca02190e1cac030be3bfd
Neboraka <oleshchukii@gmail.com>
2022-05-24 09:35:56 +0300

    DOM2 ended

--- JSC/L9DOMP2/domp2.js
+++ JSC/L9DOMP2/domp2.js
@@ -20,3 +20,31 @@
 //     li.innerText = masE
 //     elem.appendChild(li)
 // }
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+// for (const courEl of coursesAndDurationArray) {
+//     const div = document.createElement('div')
+//     div.innerHTML = `<a>${courEl.title} - ${courEl.monthDuration}</a>`
+//     document.body.appendChild(div)
+// }
+
+for (const couEl of coursesAndDurationArray) {
+    let div = document.createElement('div');
+    div.className = 'item'
+    let hEl = document.createElement('h1');
+    hEl.className = 'heading'
+    hEl.innerText = `${couEl.title}`
+    let pEl = document.createElement('p');
+    pEl.className = 'description'
+    pEl.innerText = `${couEl.monthDuration}`
+    document.body.append(hEl, pEl)
+
+}
